ef9384a9a09126e79bb7d8c9c65fd85566926a61
Minkov <momchil.minkov@softwareone.com>
2022-11-02 12:31:10 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "moksongmin1"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
